2a37a17878d5da82463dd2dec904ba1fdf436271
Hasan Čolaković <hasancolakovic32@gmail.com>
2022-07-13 12:52:29 +0200

    Kreirana klasa SpecificGenerator uz pomoć koje ćemo kreirati passworde sa lowecase-om.

---
+++ zadatak_2_net/SpecificGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadatak_2_net
+{
+    class SpecificGenerator : PasswordGenerator
+    {
+        public override string generatePassword(int size)
+        {
+            StringBuilder rezultat = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                rezultat.Append(this.getRandomCharacter());
+            }
+
+            return rezultat.ToString().ToLowerInvariant();
+        }
+    }
+}
