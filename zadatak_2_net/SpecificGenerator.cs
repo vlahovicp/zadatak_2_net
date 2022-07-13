b9d068e3dbb5ea6372722c407788a1593196b8fd
nikolapivac <npivac00@fesb.hr>
2022-07-13 13:42:35 +0200

    Added SpecificGenerator

---
+++ zadatak_2_net/SpecificGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zadatak_2_net
+{
+    internal class SpecificGenerator : PasswordGenerator
+    {
+        public override string generatePassword(int size)
+        {
+            int Counter = 0;
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            string password = null;
+
+            do
+            {
+                var RandomChar = getRandomCharacter();
+
+                if (chars.Contains(RandomChar))
+                {
+                    password += RandomChar;
+                    Counter++;
+                }
+            } while (Counter < size);
+
+            return password;
+        }
+    }
+}
