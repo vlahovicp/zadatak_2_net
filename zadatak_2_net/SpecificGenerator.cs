ce460e4bba235989377893a170dc1a1ffef1f055
Noa Midzic <nmidzic20@foi.hr>
2022-07-15 15:42:54 +0200

    Implementirana klasa SpecificGenerator

---
+++ zadatak_2_net/SpecificGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadatak_2_net
+{
+    internal class SpecificGenerator : PasswordGenerator
+    {
+        public override string generatePassword(int size)
+        {
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < size; i++)
+            {
+                char character;
+                do
+                {
+                    character = this.getRandomCharacter();
+                } while (character < 'a' || character > 'z');
+                password.Append(character);
+            }
+            return password.ToString();
+        }
+    }
+}
